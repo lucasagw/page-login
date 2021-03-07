bbb971f9d4dfe576bdc5da7e2704e82d6be47953
lucasagw <79553621+lucasagw@users.noreply.github.com>
2021-03-06 22:31:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function bomdia(){   
-             alert("Bom dia!");
+             alert('Bom dia!');
              var h1v = document.querySelector("h1");
              h1v.textContent = "UCSAL";
           }
-          
+
+function botaoEvento(){
+  var senha = document.querySelector("inputPassword");   
+  
+  if(senha.lenght < 6){
+     alert('Senha precisa ser maior que 6');
+     }
+  alert('Botão clicado');
+          }
+  
+  
+           
+
